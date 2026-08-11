ad63f068e921e490c0e7ec2e2389ab52dd117b1d
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2026-08-11 07:14:04 -0300

    feat(nix): initialize flake

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      crane,
+      fenix,
+      systems,
+      devshell,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      imports = [
+        devshell.flakeModule
+      ];
+      perSystem =
+        {
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
+        let
+          toolchain = fenix.packages.${system}.combine [
+            fenix.packages.${system}.default.toolchain
+            fenix.packages.${system}.latest.rust-src
+          ];
+          craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+          root = ./.;
+
+          args = {
+            src = lib.fileset.toSource {
+              inherit root;
+              fileset = lib.fileset.unions [
+                (craneLib.fileset.commonCargoSources root)
+                # (lib.fileset.fileFilter (file: file.hasExt "md") root)
+              ];
+            };
+            strictDeps = true;
+
+            nativeBuildInputs = [ ];
+            buildInputs = [ ];
+          };
+
+          bin = craneLib.buildPackage (
+            args
+            // {
+              cargoArtifacts = craneLib.buildDepsOnly args;
+            }
+          );
+        in
+        {
+          # checks.<package> = bin;
+
+          packages.default = bin;
+
+          devshells.default = {
+            packages = [
+              toolchain
+            ];
+
+            motd = "";
+          };
+        };
+    };
+}
